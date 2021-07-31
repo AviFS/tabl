f17310a30cec9808d2384af9ae4c8a17845c9c7e
AviFS <avifsternlieb@gmail.com>
2021-07-30 19:32:13 -0700

    Fixed issue #9, I think

--- slashes.js
+++ slashes.js
@@ -33,12 +33,15 @@ function slash(prog) {
         if (state == 1 && print!="") { logPrint(); res += print; print = ""; }
         if (state == 3) {
             var isVerbose = false;
+            var iters = 0; const MAX_ITERS = 1280;
             while (prog.includes(pattern)) {
-            prog = prog? prog.replace(pattern, replacement) : prog;
-            // Just `prog = prog.replace(pat, rep);` might be good enough
-            // Was trying to get rid of the null character that was printing
-            // Thought maybe the replacement with an empty string pattern was undefined
-            if (isVerbose) { logApply(); }
+                if (iters > MAX_ITERS) { print+='...'; break; }
+                iters++;
+                prog = prog? prog.replace(pattern, replacement) : prog;
+                // Just `prog = prog.replace(pat, rep);` might be good enough
+                // Was trying to get rid of the null character that was printing
+                // Thought maybe the replacement with an empty string pattern was undefined
+                if (isVerbose) { logApply(); }
             }
             if (!isVerbose) { logApply(); }
             pattern = ""; replacement = "";
